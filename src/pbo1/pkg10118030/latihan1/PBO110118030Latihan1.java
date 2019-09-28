2b0f40e3ecc1d62aeea654ab61b3768ebe59694a
Ari Sulistio <arisulistio37@gmail.com>
2019-09-28 14:02:13 +0700

    Biodata

---
+++ src/pbo1/pkg10118030/latihan1/PBO110118030Latihan1.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo1.pkg10118030.latihan1;
+
+/**
+ *
+ * @author 
+ * NAMA         : Ari Sulistio
+ * KELAS        : IF-1
+ * NIM          : 10118030
+ * Deskripsi Program    : Program ini berisi biodata
+ * 
+ */
+public class PBO110118030Latihan1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("BIODATA ROCKSTAR" + "\n" + "---------------");
+        System.out.println("NAMA\t\t: Ari Sulistio");
+        System.out.println("ALAMAT\t\t: Cijambe");
+        System.out.println("TEMPAT LAHIR\t: Bandung");
+        System.out.println("TANGGAL LAHIR\t: 17 Juli 2000");
+        System.out.println("---------------");
+        System.out.println("HOBI\t\t: Tidur");
+        System.out.println("MAKES\t\t: Nasgor");
+        System.out.println("MIKES\t\t: Kopi");
+        
+        
+    }
+    
+}
